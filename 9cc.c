bcb4d027e23b285ad61ba175954f83b785692281
interimadd <interimadd@gmail.com>
2021-08-11 17:03:29 +0900

    整数1つをコンパイルするコンパイラを作成

---
+++ 9cc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "引数の個数が正しくありません\n");
+        return 1;
+    }
+
+    printf(".text\n");
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("    mov r0, #%d\n", atoi(argv[1]));
+    printf("    bx lr\n");
+    return 0;
+}
