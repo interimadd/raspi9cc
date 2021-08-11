0e431967b9b4a5da32a91a23c7e8af4820e26fda
interimadd <interimadd@gmail.com>
2021-08-11 17:14:14 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -6,11 +6,29 @@ int main(int argc, char **argv) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
+    
+    char *p = argv[1];
 
     printf(".text\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov r0, #%d\n", atoi(argv[1]));
+    printf("    mov r0, #%ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add r0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            printf("    sub r0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字列です： '%c'\n", *p);
+    }
+
     printf("    bx lr\n");
     return 0;
 }
